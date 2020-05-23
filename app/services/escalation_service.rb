46069b227f1ff42711f640b188820e16ed2ad486
Djamal Sadaghiani <dj8amal@gmail.com>
2020-05-23 17:55:05 +0200

    fix

--- app/services/escalation_service.rb
+++ app/services/escalation_service.rb
@@ -3,7 +3,7 @@ class EscalationService
 
     def perform
         chat_ids = [ENV['CHAT_ID_JD'], ENV['CHAT_ID_DJ'], ENV['CHAT_ID_HEL']]
-        message = "Hello, the Bell just rang at: #{Bell.last.created_at.in_time_zone('Berlin')}"
+        message = "Hello, the Bell just rang at: #{Bell.last&.created_at&.in_time_zone('Berlin')}"
         token = ENV['BOT_TOKEN']
 
         Telegram::Bot::Client.run(token) do |bot|
